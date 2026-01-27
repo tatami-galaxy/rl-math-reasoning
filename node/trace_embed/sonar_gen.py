a61b4ef803f39e519aaea6d46ec36a3b98e7905f
Ujan Deb <ujan@unsw.testserver.com.au>
2026-01-27 04:26:27 +0000

    vf properties of learned vf. sde, latent ode, more powerful architecture, more data. sentence embed for sonar.

--- node/trace_embed/sonar_gen.py
+++ node/trace_embed/sonar_gen.py
@@ -3,6 +3,7 @@ sys.path.append('../../')
 from dataclasses import dataclass, field
 from utils import get_root_dir
 
+import re
 import time
 import torch
 import numpy as np
@@ -30,10 +31,44 @@ class SonarHyps:
     dataset_split: str = field(default="train")
     sample: bool = field(default=False)
     num_samples: int = field(default=500)
-    # TODO : sentence instead since sonar does not work with long text?
-    segment_by: str = field(default="\n\n")
-    min_segment_chars: str = field(default=50)
-    min_num_segments: int = field(default=10)
+    min_sent_chars: str = field(default=15)
+    min_num_sents: int = field(default=10)
+
+
+def merge_small_sentences(config, sentences):
+    valid_sentences = []
+    for sentence in sentences:
+        # push first sentence
+        if len(valid_sentences) == 0:
+            valid_sentences.append(sentence)
+        else:
+            if len(valid_sentences[-1]) < config.min_sent_chars:
+                # we have already removed trailing whitespaces
+                valid_sentences[-1] += ' ' + sentence
+            else:
+                valid_sentences.append(sentence)
+    return valid_sentences
+
+
+# splits trace into sentences
+def get_sentences(config, trace):
+    # (?<![.?!:]) -> Negative lookbehind: Don't match if preceded by another punctuation mark
+    # [.?!:]      -> Character class: Match any one of these four characters
+    # (?=\s|$)    -> Positive lookahead: Match only if followed by space or end of string
+    pattern = r'(?<![.?!:])[.?!:](?=\s|$)'
+    sentences = re.split(pattern, trace)
+    # clean up whitespace
+    sentences = [s.strip() for s in sentences if s.strip()]
+    # merge small sentences
+    sentences = merge_small_sentences(config, sentences)
+    return sentences
+
+
+# TODO : reconstruct sentences
+# if reconstruction poor, split large senteces until reconstruction acceptable
+def validate_sentences(config, sentences, t2vec_model, vec2text_model):
+    pass
+
 
 
 if __name__ == "__main__":
@@ -54,7 +89,7 @@ if __name__ == "__main__":
         remove_columns=trace_dataset.column_names,
     ).shuffle(config.seed)
 
-    # TODO : sonar -> sentence as segment?
+    # sonar models
     t2vec_model = TextToEmbeddingModelPipeline(
         encoder="text_sonar_basic_encoder",
         tokenizer="text_sonar_basic_encoder",
@@ -67,42 +102,16 @@ if __name__ == "__main__":
         device=torch.device("cuda"),
         dtype=torch.float16,
     )
-    segs = trace_dataset[0]['trace'].split(config.segment_by)
-    #print(segs[1])
-    #print('\n\n')
-    embeddings = t2vec_model.predict(["T(n) defined by T(n) = T(n-1) + n² + n² log n. Hmm, let's start by understanding what this function does."], source_lang="eng_Latn")
+
+    # example
+    trace = trace_dataset[0]['trace']
+    sentences = get_sentences(config, trace)
+    embeddings = t2vec_model.predict(["n²(1 + log n) ≤ C [n³ log n - (n - 1)^3 log n]"], source_lang="eng_Latn")
     print(embeddings.shape)
     print('\n\n')
     reconstructed = vec2text_model.predict(embeddings, target_lang="eng_Latn", max_seq_len=256)
     print(reconstructed)
     quit()
-
-
-    segment_representations(root, config)
-
-    # get segment representations
-    # list of tensors with different lengths
-    data, metadatas = load_segment_representations(root, config)
-    print('Num examples : {}'.format(len(data)))
-    print('Avg num segments : {}'.format(int(sum([d.shape[0] for d in data])/len(data))))
-
-    # lower data dimensionality before training
-    if config.proj_before_train:
-        print('PCA before training...')
-        pca, data = pca_trace(data, num_components=config.proj_dim)
-    
-    # node and optimizer
-    model = NeuralODE(
-        config.node_width, config.node_depth, data[0].shape[1], 
-        config.pid_rtol, config.pid_atol, key=model_key
-    )
-    optim = optax.adabelief(config.lr)
-
-    # train
-    # either integrate per trajectory -> this is being done now!
-    # or common time stamps, mask invalid time stamps
-    # TODO : or latent ode
-    train(root, config, model, data, optim, loader_key)
 
 
 
